8f7e3a79b999aa7b39d96d3f26334389ff719b21
Generaleyebrows <michelh1101@gmail.com>
2024-07-06 22:31:42 -0500

    Ok, maybe some progress ??

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pygame
-import time
 from sys import exit
-import math
 
 pygame.init()
 
@@ -19,20 +17,7 @@ rect_1 = pygame.Rect(300,display_h-50,50,50)
 
 class MainEvent():
     
-    def __init__(self):
-        
-        self.gravity_var = 0.01
-        self.t0 = time.time() 
-    
-    def trajectory(self, x, g, v, theta): # gravity, velocity, angle
-        return x*math.tan(theta) - ((g*math.pow(x, 2))/(2*math.pow(v, 2))*(math.pow(math.cos(theta), 2)))
-    
     def Main(self):
-        
-        traj_x = 0 # trajectory coordinates for rect_1.move_ip()
-        traj_y = 0
-        velocity = 0.01
-        angle = 0
         
         moving = False
         
@@ -54,34 +39,21 @@ class MainEvent():
                     
                     rect_1.move_ip(event.rel)
                     
-                    self.gravity_var = -0.0981
-                    
-                    dx = event.rel[0] # created for initial velocity
-                    mx, my = event.pos # coordinates for arctan to create angle
-                    real_my = display_h - my # my is opposite is movement to a normal graph, so it's reversed
-                    
-                    print(f"mx = {mx}")
-                    print(f"my = {real_my}")
-                    print(f"angle = {math.atan2(real_my, mx)}")
+                    dx, dy = event.rel # change in mouse movement for one frame
                     
             if rect_1.y < display_h-50 and moving == False: # rectangle is let go, trajectory is calculated
                 
-                velocity = abs(dx)+0.01 # stop it from ever being 0
-                    
-                angle = math.atan2(real_my, mx)
-                                
-                t1 = time.time()
-
-                if t1 - self.t0 >= 0.01: # if time elapsed > 10 milisecond
-                    traj_x = rect_1.x*0.01
-                    self.gravity_var-=9.81*0.01
-                    traj_y = self.trajectory(traj_x, self.gravity_var, velocity, angle)
-                    rect_1.move_ip(traj_x, traj_y)
-                    
-                    self.t0 = t1      
+                gravity = 9.81
+                velocity_vec_x = dx
+                velocity_vec_y = dy/60
+                
+                velocity_vec_x += velocity_vec_x
+                velocity_vec_y += gravity
+                
+                rect_1.move_ip(velocity_vec_x, velocity_vec_y)
 
             pygame.display.update()
             clock.tick(60)
+       
             
-
 MainEvent().Main()
